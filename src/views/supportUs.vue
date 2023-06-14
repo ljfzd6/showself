19b8d70c128ed254f8f9460b6ce5b8a9dec00b6e
Amlenc <1582439289@qq.com>
2023-06-14 16:58:51 +0800

    new file:   src/assets/AliPay_Full.jpg 	modified:   src/views/supportUs.vue

--- src/views/supportUs.vue
+++ src/views/supportUs.vue
@@ -2,29 +2,28 @@
     <div>
         <el-descriptions class="margin-top" title="赞助，求求，谢谢！" :column="3" :size="size" border>
             <template slot="extra">
-            <el-button type="primary" size="small" @click="dialogFormVisible1= true">打款一百万！</el-button>
-            <el-button type="primary" size="small" @click="dialogFormVisible2= true">赏你一毛QAQ </el-button>
+            <el-button type="primary" size="small" @click="dialogFormVisible1= true">支付宝打款一百万！</el-button>
+            <el-button type="primary" size="small" @click="dialogFormVisible2= true">微信赏你一毛QAQ </el-button>
             </template>
-
-            <el-descriptions-item>
-                <p>阿里嘎多思密达！<p>
-            </el-descriptions-item>
-            
         </el-descriptions>
+
+        <template>
+            如果您喜欢我的作品，简单的捐赠是对我最大的物质和精神支持~谢谢您!
+        </template>
 
         <!-- 修改密码弹窗 -->
         <el-dialog title="支付宝赏饭" :visible.sync="dialogFormVisible1">
-            <el-image style="width: 100px; height: 100px" :src="@/assets/background.jpg" :fit="fit"></el-image>
+            <img  style="width: 250px; " src="../assets/AliPay_Full.jpg"   /> 
             <div slot="footer" class="dialog-footer">
-                <el-button type="primary" @click="dialogFormVisible1= true">老板大气！</el-button>
+                <el-button type="primary" @click="dialogFormVisible1= false">老板大气！</el-button>
             </div>
         </el-dialog>
 
         <!-- 修改密码弹窗 -->
         <el-dialog title="微信赏饭" :visible.sync="dialogFormVisible2">
-            <el-image style="width: 100px; height: 100px" :src="@/assets/background.jpg" :fit="fit"></el-image>
+            <img  style="width: 250px; " src="../assets/AliPay_Full.jpg"   /> 
             <div slot="footer" class="dialog-footer">
-                <el-button type="primary" @click="dialogFormVisible2= true">老板大气！</el-button>
+                <el-button type="primary" @click="dialogFormVisible2= false">老板大气！</el-button>
             </div>
         </el-dialog>
     </div>
